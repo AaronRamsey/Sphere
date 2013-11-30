92b4210b4bf5465498145bda2b0f2c72e408e72e
Alex Koren <alex.koren@jhu.edu>
2013-11-30 11:29:17 -0500

    Initial Commit

---
+++ Sphere/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sphere
+//
+//  Created by Alex Koren on 11/30/13.
+//  Copyright (c) 2013 AKApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SphereTests/SphereTests.m
@@ -0,0 +1,34 @@
+//
+//  SphereTests.m
+//  SphereTests
+//
+//  Created by Alex Koren on 11/30/13.
+//  Copyright (c) 2013 AKApps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SphereTests : XCTestCase
+
+@end
+
+@implementation SphereTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
